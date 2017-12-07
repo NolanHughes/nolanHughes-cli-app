ddb335b4e57fd94890ae28d051a63d1e36b85390
Nolan Hughes <nhughes987@gmail.com>
2017-12-07 20:44:08 +0000

    Expand on functionality of cli. Put in dumby information

--- lib/nolanHughes_cli_app/cli.rb
+++ lib/nolanHughes_cli_app/cli.rb
@@ -3,6 +3,34 @@ class NolanHughesCliApp::CLI
 
   def call
     puts "Welcome to Breweries Near You!"
+    list_breweries
+    menu
+    goodbye
   end
 
+  def list_breweries
+    puts <<-DOC.gsub /^\s*/, ''
+      1. Begyle Brewery
+      2. Half Acre Brewing Company
+      3. Pipeworks Brewing Company
+    DOC
+  end
+
+  def menu
+    puts "Enter the number or name of the brewery you'd like more info on or type exit:"
+    # input = gets.strip.downcase
+    # while input != "exit"
+    #
+    #   case input
+    #   when "1"
+    #     puts "More info on brewery 1..."
+    #   when "2"
+    #     puts "More info on brewery 2..."
+    #   end
+    # end
+  end
+
+  def goodbye
+    puts "Until next time!"
+  end
 end
