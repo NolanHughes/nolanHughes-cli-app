4cf8821a0c155bd1690c67f32109b770368a9ec4
Nolan Hughes <nhughes987@gmail.com>
2017-12-08 20:37:45 +0000

    Add pry. make cli flow through menus better

--- lib/breweries_near_me/cli.rb
+++ lib/breweries_near_me/cli.rb
@@ -1,12 +1,11 @@
 #Our CLI Controller
+require 'pry'
 class BreweriesNearMe::CLI
 
   def call
     puts "Welcome to Breweries Near You!"
     list_cities
     city_menu
-    # brewery_menu
-    goodbye
   end
 
   def list_cities
@@ -14,24 +13,28 @@ class BreweriesNearMe::CLI
   end
 
   def make_and_print_breweries(city)
-    puts "Here are the breweries from that city"
+    puts "Here are the breweries from #{city}"
     @breweries = BreweriesNearMe::Brewery.create_breweries(city)
     @brewery_names = BreweriesNearMe::Brewery.print_breweries
   end
 
   def city_menu
     input = nil
-    while input != "exit"
+    if input != "exit"
       #maybe increase name flexability and add number
       puts "What city do you want to find breweries in? You can also type list to see all of the cities again, or type exit to leave:"
       input = gets.strip.downcase
       case input
-      when "chicago" || 1
+      when "chicago"
         make_and_print_breweries(input)
-      when "fort collins" || 2
+        brewery_menu
+      when "fort collins"
         make_and_print_breweries(input)
+        brewery_menu
       when "list"
         list_cities
+      when "exit"
+        goodbye
       else
         puts "Not sure what you want. Please type list or exit."
       end
@@ -42,7 +45,7 @@ class BreweriesNearMe::CLI
     input = nil
     while input != "exit"
       #maybe increase name flexability and add number
-      puts "Enter the number of the brewery you'd like more info on, type list to see all breweries again, or type exit to leave:"
+      puts "Enter the name of the brewery you'd like more info on, type list to see all breweries again, or type exit to leave:"
       input = gets.strip.downcase
       case input
       when "list"
