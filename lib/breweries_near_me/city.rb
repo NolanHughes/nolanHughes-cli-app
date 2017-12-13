c446af47cd9f59cce63cf801bf15fc4809476a31
Nolan Hughes <nhughes987@gmail.com>
2017-12-13 16:11:34 +0000

    Add dependency for rest-client gem

--- lib/breweries_near_me/city.rb
+++ lib/breweries_near_me/city.rb
@@ -22,30 +22,31 @@ class BreweriesNearMe::City
   end
 
   def self.create_cities
-    self.scrape_cities
+    @city_1 = self.new
+    @city_1.name =  "Chicago"
+    @@all_cities << @city_1
+
+    @city_2 = self.new
+    @city_2.name =  "Fort Collins"
+    @@all_cities << @city_2
+
     add_breweries
     all
-        # @city_1 = self.new
-        # @city_1.name =  "Chicago"
-        # @@all_cities << @city_1
-        #
-        # @city_2 = self.new
-        # @city_2.name =  "Fort Collins"
-        # @@all_cities << @city_2
-  end
-
-  def self.scrape_cities
-    #go to beeradvocate, find the name of the city
-    #instantiate a city
-    #push city to @@all_cities
-    @@top_beer_cities.each do |state, city|
-      doc = Nokogiri::HTML(open("https://www.beeradvocate.com/place/list/?&c_id=US&s_id=#{state}&city=#{city}&sort=name&sort=avgbeers"))
-      binding.pry
-      city =
-      @@all_cities << city
-    end
 
   end
+
+  # def self.scrape_cities
+  #   #go to beeradvocate, find the name of the city
+  #   #instantiate a city
+  #   #push city to @@all_cities
+  #   @@top_beer_cities.each do |state, city|
+  #     doc = Nokogiri::HTML(open("https://www.beeradvocate.com/place/list/?&c_id=US&s_id=#{state}&city=#{city}&sort=name&sort=avgbeers"))
+  #     binding.pry
+  #     city =
+  #     @@all_cities << city
+  #   end
+  #
+  # end
 
   def self.add_breweries
     all.each do |city|
