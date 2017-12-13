f42dcface66f75836c5b5e500f5c6cc2e3749156
Nolan Hughes <nhughes987@gmail.com>
2017-12-13 15:58:17 +0000

    Take out requires for nokogiri and uri in environment file.

--- lib/breweries_near_me.rb
+++ lib/breweries_near_me.rb
@@ -1,9 +1,9 @@
 #delte pry before publishing
-require 'open-uri'
 require 'pry'
-require 'nokogiri'
+require 'rest-client'
+require 'json'
 
-require_relative "breweries_near_me/version"
+require_relative "./breweries_near_me/version"
 require_relative './breweries_near_me/cli'
 require_relative './breweries_near_me/breweries'
 require_relative './breweries_near_me/city'
