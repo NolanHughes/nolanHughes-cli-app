781f920864cce4a5f3ffc200a06615a8c2b9701b
Nolan Hughes <nhughes987@gmail.com>
2017-12-13 00:53:33 +0000

    Start scrape method in City class. Might be too hard, so might change to an api.

--- lib/breweries_near_me.rb
+++ lib/breweries_near_me.rb
@@ -1,3 +1,8 @@
+#delte pry before publishing
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
 require_relative "breweries_near_me/version"
 require_relative './breweries_near_me/cli'
 require_relative './breweries_near_me/breweries'
--- lib/breweries_near_me/city.rb
+++ lib/breweries_near_me/city.rb
@@ -1,17 +1,18 @@
 class BreweriesNearMe::City
   attr_accessor :name, :breweries
 
+  #maybe scrape data to make this list?
   @@top_beer_cities= {
     "OR" => "Portland",
     "CA" => "San Diego",
     "CO" => "Denver",
     "WA" => "Seattle",
     "IL" => "Chicago",
-    "CA" => "San Francisco",
     "NY" => "NYC",
     "MI" => "Grand Rapids",
     "WI" => "Milwaukee",
-    "MN" => "Minneapolis"
+    "MN" => "Minneapolis",
+    "NC" => "Asheville"
 }
 
   @@all_cities = []
@@ -23,24 +24,27 @@ class BreweriesNearMe::City
   def self.create_cities
     self.scrape_cities
     add_breweries
-    # [@city_1, @city_2] #leave here? or call all? I think call all. Seems better.
+    all
+        # @city_1 = self.new
+        # @city_1.name =  "Chicago"
+        # @@all_cities << @city_1
+        #
+        # @city_2 = self.new
+        # @city_2.name =  "Fort Collins"
+        # @@all_cities << @city_2
   end
 
   def self.scrape_cities
     #go to beeradvocate, find the name of the city
     #instantiate a city
     #push city to @@all_cities
+    @@top_beer_cities.each do |state, city|
+      doc = Nokogiri::HTML(open("https://www.beeradvocate.com/place/list/?&c_id=US&s_id=#{state}&city=#{city}&sort=name&sort=avgbeers"))
+      binding.pry
+      city =
+      @@all_cities << city
+    end
 
-
-    # @city_1 = self.new
-    # @city_1.name =  "Chicago"
-    # @@all_cities << @city_1
-    #
-    # @city_2 = self.new
-    # @city_2.name =  "Fort Collins"
-    # @@all_cities << @city_2
-    #
-    # [@city_1, @city_2]
   end
 
   def self.add_breweries
@@ -50,3 +54,5 @@ class BreweriesNearMe::City
   end
 
 end
+
+#doc.css("#ba-content td.hr_bottom_dark").last.text.split[3]
