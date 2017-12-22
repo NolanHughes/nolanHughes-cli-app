fdf5150ea4eeb5f1c3740064c357e416b0695af2
NolanHughes <Nhughes987@gmail.com>
2017-12-22 16:35:09 -0600

    Delete unnecessary comments.

--- lib/breweries_near_me/scraper.rb
+++ lib/breweries_near_me/scraper.rb
@@ -7,7 +7,6 @@ class BreweriesNearMe::Scraper
     brewery_table_rows = doc.css('#ba-content table tr')
 
     breweries_that_have_ratings = brewery_table_rows[3..-1].collect do |row|
-      #first 3 rows are garbage
       name_and_ratings = row.css('b').collect do |text|
         text.text
       end
@@ -15,10 +14,7 @@ class BreweriesNearMe::Scraper
       if name_and_ratings[3] != "-" && name_and_ratings[4].to_i > 10
         name_and_ratings
       end
-
     end
-
     top_ten_breweries = breweries_that_have_ratings.reject { |brewery| brewery.nil? || brewery.empty?}[0..9]
-    binding.pry
   end
 end
