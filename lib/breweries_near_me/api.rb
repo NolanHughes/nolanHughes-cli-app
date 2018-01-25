ea84cc70adf8e5099acec2db39cf0c95b8eace83
NolanHughes <Nhughes987@gmail.com>
2018-01-25 17:40:06 -0600

    Add key.rb and make an env variable for inserting api key from BreweryDB.

--- lib/breweries_near_me/api.rb
+++ lib/breweries_near_me/api.rb
@@ -20,10 +20,8 @@ class BreweriesNearMe::API
   end
 
   def get_all_brewery_info(input)
-    api_key = KEY[:api_key]
-    binding.pry
     #Should I hide my api key somehow? ***FIGURE THIS OUT***
-    all_breweries_url = "https://api.brewerydb.com/v2/locations?locality=#{input}&key=#{api_key}&format=json"
+    all_breweries_url = "https://api.brewerydb.com/v2/locations?locality=#{input}&key=#{ENV["API_KEY"]}&format=json"
 
     brewery_list = RestClient.get(all_breweries_url)
     parsed_list = JSON.parse(brewery_list)
@@ -52,8 +50,7 @@ class BreweriesNearMe::API
   end
 
   def get_beer_from_api(the_brewery_id)
-    api_key = KEY[:api_key]
-    beer_url = "https://api.brewerydb.com/v2/brewery/#{the_brewery_id}/beers?key=#{api_key}&format=json"
+    beer_url = "https://api.brewerydb.com/v2/brewery/#{the_brewery_id}/beers?key=#{ENV["API_KEY"]}&format=json"
 
     beer_list = RestClient.get(beer_url)
     parsed_list = JSON.parse(beer_list)
